1f5383aa5e180fa5c1287667f0f32d23db7bedff
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-03-18 18:13:46 -0300

    remove log

--- src/controllers/DataController.js
+++ src/controllers/DataController.js
@@ -3,13 +3,11 @@ const mdsHelper = require('../helpers/mdsHelper')
 
 class DataController {
   async index(req, res) {
-    console.log('controllers')
     const {data} = await mdsService.get(
       `/novocoronavirus/resources/scripts/database.js?v=${new Date().getTime()}`
     )
     const dataParsed = mdsHelper.sourceParser(data)
-    console.log(data)
-    console.log(dataParsed)
+
     return res.json(dataParsed)
   } 
 }
